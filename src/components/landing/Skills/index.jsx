95beb2cb6520cb6b20eef9a3b427dbd41f13fdd6
Kamil Kiedrzyn <kkiedrzyn@efigence.com>
2020-10-15 21:47:19 +0200

    Setup

--- src/components/landing/Skills/index.jsx
+++ src/components/landing/Skills/index.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 import { ThemeContext } from 'providers/ThemeProvider';
 import { Container, Button } from 'components/common';
-import dev from 'assets/illustrations/skills.svg';
+import dev from 'assets/illustrations/Landlord_appointments.svg';
 import { Wrapper, SkillsWrapper, Details, Thumbnail } from './styles';
 
 export const Skills = () => {
@@ -15,10 +15,9 @@ export const Skills = () => {
           <img src={dev} alt="I’m John and I’m a Backend & Devops engineer!" />
         </Thumbnail>
         <Details theme={theme}>
-          <h1>More about me</h1>
+          <h1>Oglądanie mieszkania online</h1>
           <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-            industry’s standard dummy.
+            Umawiaj się na oglądanie mieszkania za pośrednictwem aplikacji wybierając termin z kalendarza dostępności właściciela.
           </p>
           <Button as={AnchorLink} href="#contact">
             Hire me
