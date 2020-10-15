071182a3c3157b3b5dfc807baded154543617d9c
Kamil Kiedrzyn <kkiedrzyn@efigence.com>
2020-10-15 22:53:01 +0200

    Remove fukkin project component

--- src/components/landing/Skills/index.jsx
+++ src/components/landing/Skills/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import dev from 'assets/illustrations/Landlord_appointments.svg';
+import dev2 from 'assets/illustrations/Landlord_payments.svg';
 import {DoubleBox} from 'components/common/DoubleBox';
 
 export const Skills = () => {
@@ -22,7 +23,7 @@ export const Skills2 = () => {
   return (
     <DoubleBox
       imgLeft={false}
-      imgSrc={dev}
+      imgSrc={dev2}
       detailsContent={
         <>
           <h1>Oglądanie mieszkania online</h1>
@@ -33,22 +34,4 @@ export const Skills2 = () => {
       }
     />
   )
-};
-
-
-export const Skills3 = () => {
-    return (
-        <DoubleBox
-            imgLeft={false}
-            imgSrc={dev}
-            detailsContent={
-                <>
-                    <h1>Oglądanie mieszkania online</h1>
-                    <p>
-                        Umawiaj się na oglądanie mieszkania za pośrednictwem aplikacji wybierając termin z kalendarza dostępności właściciela.
-                    </p>
-                </>
-            }
-        />
-    )
 };
