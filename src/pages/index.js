5ce5cb1102fecd2ca060ce7eb0f2769fb830eeb2
Jacek Grzybowski <jacek213@gmail.com>
2020-10-20 22:33:42 +0200

    cleanup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Layout, SEO } from 'components/common';
-import { Intro, Contact } from 'components/landing';
-import {OwnerOrRenter} from 'components/landing/OwnerOrRenter';
+import {Intro, Contact, OwnerOrRenter } from 'components/landing';
 
 
 export default () => (
