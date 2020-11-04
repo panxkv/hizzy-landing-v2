5c11add3265140487769bbdb4869472446cd0eea
Jacek Grzybowski <jgrzybowski@efigence.com>
2020-11-04 22:56:27 +0100

    copy improvments

--- src/components/landing/OwnerOrRenter/TenantInfo/index.jsx
+++ src/components/landing/OwnerOrRenter/TenantInfo/index.jsx
@@ -15,8 +15,7 @@ export const TenantInfo = () => (
         <>
           <h1>Umawiaj się na oględziny przez kalendarz online</h1>
           <p>
-            Koniec ze żmudnymi telefonami. Zapisuj się na oględziny mieszkania za pośrednictwem aplikacji, wybierając dogodny termin z kalendarza dostępności
-            właściciela.
+            Koniec ze żmudnymi telefonami. Zapisuj się na oględziny mieszkania za pośrednictwem aplikacji, wybierając dogodny termin z kalendarza dostępności właściciela.
           </p>
         </>
       }
@@ -27,7 +26,7 @@ export const TenantInfo = () => (
       detailsContent={
         <>
           <h1>Zobacz mieszkanie bez wychodzenia z domu</h1>
-          <p>Nie możesz pojawić się osobiście? Bierz udział w wirtualnych oględzinach mieszkania poprzez połączenie wideo z właścicielem lub spaceruj po mieszkaniu korzystając z wizualizacji 3D.</p>
+          <p>Nie możesz pojawić się osobiście? Bierz udział w wirtualnych oględzinach mieszkania poprzez połączenie wideo z właścicielem lub spaceruj po mieszkaniu korzystając z technologii VR 360.</p>
         </>
       }
     />
@@ -36,7 +35,7 @@ export const TenantInfo = () => (
       detailsContent={
         <>
           <h1>Podpisz umowę online</h1>
-          <p>Po przespaniu się z decyzją stwierdziłeś, że znalazłeś "to wymarzone" mieszkanie? Poinformuj o tym właściciela jednym kliknięciem, a otrzymasz treść umowy do podpisu elektronicznego.</p>
+          <p>Stwierdziłeś, że znalazłeś wymarzone mieszkanie? Poinformuj o tym właściciela jednym kliknięciem, a otrzymasz treść umowy do podpisu elektronicznego.</p>
         </>
       }
     />
@@ -46,7 +45,7 @@ export const TenantInfo = () => (
       detailsContent={
         <>
           <h1>Płać czynsz i swoje rachunki online</h1>
-          <p>Otrzymuj comiesięczne rachunki za czynsz od właściciela wewnątrz aplikacji. Następnie opłacać je w kilka sekund poprzez zintegrowane płatności online.</p>
+          <p>Otrzymuj comiesięczne rachunki za czynsz od właściciela wewnątrz aplikacji. Następnie opłać je w kilka sekund poprzez zintegrowane płatności online.</p>
         </>
       }
     />
@@ -56,7 +55,7 @@ export const TenantInfo = () => (
       detailsContent={
         <>
           <h1>Komunikuj się z właścicielem przez aplikację</h1>
-          <p>Zepsuty kran? A może nie jesteś pewien, czy możesz wywiercić dziurę w ścianie by powiesić swój nowy obraz? Skontaktuj się z właścicielem przy pomocy wbudowanego komunikatora tekstowego.</p>
+          <p>Zepsuty kran? A może nie jesteś pewien, czy możesz wywiercić dziurę w ścianie? Skontaktuj się z właścicielem przy pomocy wbudowanego komunikatora.</p>
         </>
       }
     />
